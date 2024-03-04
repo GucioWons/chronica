5953f87829c8c217ee65c217ad6c0963f98a6e23
linux <goxer@o2.pl>
2024-03-04 20:02:48 +0100

    user module init

---
+++ user/src/test/java/pl/gucio/enzo/chronica/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gucio.enzo.chronica.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
