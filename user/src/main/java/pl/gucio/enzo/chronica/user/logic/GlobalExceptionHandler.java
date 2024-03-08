4d41f8156bd46e4416efeff5dae4c4760e093640
Kuba <goxer@o2.pl>
2024-03-08 18:13:35 +0100

    exception changes

--- user/src/main/java/pl/gucio/enzo/chronica/user/logic/GlobalExceptionHandler.java
+++ user/src/main/java/pl/gucio/enzo/chronica/user/logic/GlobalExceptionHandler.java
@@ -9,25 +9,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import pl.gucio.enzo.chronica.user.data.dto.response.ErrorResponseDto;
-
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<ErrorResponseDto> handleSQLException(SQLException exception, WebRequest webRequest){
-        final ErrorResponseDto errorResponseDto = new ErrorResponseDto("user already exist",
-                                                  webRequest.getDescription(false),
-                                                  HttpStatus.BAD_REQUEST,
-                                                  LocalDateTime.now());
-        LOGGER.error(exception.getMessage() + " at " + errorResponseDto.at());
-
-        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(IllegalArgumentException exception, WebRequest webRequest){
         final ErrorResponseDto errorResponseDto = new ErrorResponseDto(exception.getMessage(),
