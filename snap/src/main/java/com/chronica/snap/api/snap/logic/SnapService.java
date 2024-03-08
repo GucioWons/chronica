bc8d334bef8a2c17f89d7798afbb3eba614d4e55
GucioWons <wgutek1@wp.pl>
2024-03-08 18:51:25 +0100

    Create-and-get-snap

--- snap/src/main/java/com/chronica/snap/api/snap/logic/SnapService.java
+++ snap/src/main/java/com/chronica/snap/api/snap/logic/SnapService.java
@@ -2,6 +2,8 @@ package com.chronica.snap.api.snap.logic;
 
 import com.chronica.snap.api.snap.data.SnapRepository;
 import com.chronica.snap.api.snap.dto.SnapDTO;
+import com.chronica.snap.api.snap.mapper.SnapMapper;
+import jakarta.persistence.NoResultException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SnapService {
     private final SnapRepository snapRepository;
+    private final SnapMapper snapMapper;
 
     public SnapDTO getSnapById(Long id) {
-        return null;
+        return snapRepository.findById(id)
+                .map(snapMapper::mapToDTO)
+                .orElseThrow(NoResultException::new);
     }
 
-    public SnapDTO createSnap(SnapDTO snap) {
-        return null;
+    public SnapDTO createSnap(SnapDTO dto) {
+        return snapMapper.mapToDTO(snapRepository.save(snapMapper.mapToSaveEntity(dto)));
     }
 }
