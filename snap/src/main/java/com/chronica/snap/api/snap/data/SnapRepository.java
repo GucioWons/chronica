4736878bde9897b63517e2921bd99c126f4a073d
GucioWons <wgutek1@wp.pl>
2024-03-08 18:11:57 +0100

    Snap-entity

--- snap/src/main/java/com/chronica/snap/api/snap/data/SnapRepository.java
+++ snap/src/main/java/com/chronica/snap/api/snap/data/SnapRepository.java
@@ -1,7 +1,9 @@
 package com.chronica.snap.api.snap.data;
 
+import com.chronica.snap.api.snap.entity.Snap;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SnapRepository {
+public interface SnapRepository extends JpaRepository<Snap,Long> {
 }
--- snap/src/main/java/com/chronica/snap/api/snap/entity/Snap.java
+++ snap/src/main/java/com/chronica/snap/api/snap/entity/Snap.java
@@ -1,5 +1,9 @@
 package com.chronica.snap.api.snap.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +16,10 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Snap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer time;
     private Long chainId;
