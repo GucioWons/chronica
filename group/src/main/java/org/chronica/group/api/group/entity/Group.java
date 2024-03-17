878a27961479d012c4413a1c7ce562302d851852
GucioWons <wgutek1@wp.pl>
2024-03-17 13:00:49 +0100

    Implementing-h2-database

--- group/src/main/java/org/chronica/group/api/group/entity/Group.java
+++ group/src/main/java/org/chronica/group/api/group/entity/Group.java
@@ -1,13 +1,18 @@
 package org.chronica.group.api.group.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "my_group")
 public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
