8954c89b81b7e3eada0a367a7b7afc49951988c2
GucioWons <wgutek1@wp.pl>
2024-07-03 19:46:44 +0200

    Implemented-base-mapper-in-project-mapper

--- chronica-library/commons/src/main/java/org/chronica/library/commons/mapper/BaseMapper.java
+++ chronica-library/commons/src/main/java/org/chronica/library/commons/mapper/BaseMapper.java
@@ -3,7 +3,7 @@ package org.chronica.library.commons.mapper;
 public interface BaseMapper<Entity, EntityDTO> {
     EntityDTO mapToDTO(Entity entity);
 
-    Entity mapToNewEntity(EntityDTO entityDTO);
+    Entity mapToNewEntity(EntityDTO dto);
 
-    Entity mapToUpdateEntity(Entity entity, EntityDTO entityDTO);
+    Entity mapToUpdateEntity(Entity toUpdate, EntityDTO dto);
 }
