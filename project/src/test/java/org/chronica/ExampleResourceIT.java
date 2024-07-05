70a3687c37441f9d9fbc4c55e628d805a00b871e
Jakub Bartuś <141475592+BenzoEnzo@users.noreply.github.com>
2024-07-05 07:33:29 +0200

    S/CHRON-82/apigateway/quarkus-modules-fix (#18)

--- project/src/test/java/org/chronica/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.chronica;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
