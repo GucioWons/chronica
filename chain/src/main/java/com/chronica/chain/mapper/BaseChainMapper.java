0d44b08bd849257da2cc777d1bb342b5386702e6
GucioWons <wgutek1@wp.pl>
2024-07-11 20:52:09 +0200

    Implemented-mapstruct-in-chain-mappers

--- chain/src/main/java/com/chronica/chain/mapper/BaseChainMapper.java
+++ chain/src/main/java/com/chronica/chain/mapper/BaseChainMapper.java
@@ -1,19 +1,10 @@
 package com.chronica.chain.mapper;
 
-import org.chronica.library.chain.dto.BaseChainDTO;
 import com.chronica.chain.entity.Chain;
-import org.springframework.stereotype.Component;
+import org.chronica.library.chain.dto.BaseChainDTO;
+import org.mapstruct.Mapper;
 
-@Component
-public class BaseChainMapper {
-    public BaseChainDTO mapBaseChainToDTO(Chain chain) {
-        if (chain == null) {
-            return null;
-        }
-        BaseChainDTO dto = new BaseChainDTO();
-        dto.setId(chain.getId());
-        dto.setTitle(chain.getTitle());
-        dto.setType(chain.getType());
-        return dto;
-    }
+@Mapper(componentModel = "spring")
+public interface BaseChainMapper {
+    BaseChainDTO mapBaseChainToDTO(Chain chain);
 }
