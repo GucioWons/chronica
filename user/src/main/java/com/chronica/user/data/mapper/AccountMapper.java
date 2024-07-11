b901d8ea2cb9b05953d7107f02cefd4405a65102
GucioWons <wgutek1@wp.pl>
2024-07-11 21:30:14 +0200

    Implemented-mapstruct-in-user-mappers

--- user/src/main/java/com/chronica/user/data/mapper/AccountMapper.java
+++ user/src/main/java/com/chronica/user/data/mapper/AccountMapper.java
@@ -1,42 +1,28 @@
 package com.chronica.user.data.mapper;
 
+import com.chronica.user.data.entity.Account;
 import org.chronica.library.commons.exception.NotImplementedException;
 import org.chronica.library.commons.mapper.BaseMapper;
 import org.chronica.library.user.dto.AccountDTO;
-import com.chronica.user.data.entity.Account;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-@RequiredArgsConstructor
-public class AccountMapper implements BaseMapper<Account, AccountDTO> {
-    private final PersonMapper personMapper;
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = PersonMapper.class
+)
+public abstract class AccountMapper implements BaseMapper<Account, AccountDTO> {
 
-    @Override
-    public AccountDTO mapToDTO(Account account) {
-        AccountDTO dto = new AccountDTO();
-        dto.setId(account.getId());
-        dto.setUsername(account.getUsername());
-        dto.setMail(account.getMail());
-        dto.setPhoneNumber(account.getPhoneNumber());
-        dto.setActive(account.isActive());
-        dto.setDeprecated(account.isDeprecated());
-        dto.setRole(account.getRole());
-        dto.setCreatedAt(account.getCreatedAt());
-        dto.setPerson(personMapper.mapToDTO(account.getPerson()));
-        return dto;
-    }
+    public abstract AccountDTO mapToDTO(Account account);
 
-    @Override
-    public Account mapToNewEntity(AccountDTO accountDTO) {
-        Account account = new Account();
-        account.setUsername(accountDTO.getUsername());
-        account.setMail(accountDTO.getMail());
-        account.setPhoneNumber(accountDTO.getPhoneNumber());
-        account.setPassword(accountDTO.getPassword());
-        account.setPerson(personMapper.mapToNewEntity(accountDTO.getPerson()));
-        return account;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "deprecated", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    public abstract Account mapToNewEntity(AccountDTO accountDTO);
 
     //TODO updating account
     @Override
