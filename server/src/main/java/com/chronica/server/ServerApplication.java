a69afd47cae41d3f7cdaee01083c3dbb7ace4dae
Kuba <goxer@o2.pl>
2024-07-24 21:32:36 +0200

    preparation for authentication

--- server/src/main/java/com/chronica/server/ServerApplication.java
+++ server/src/main/java/com/chronica/server/ServerApplication.java
@@ -2,10 +2,12 @@ package com.chronica.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCaching
 public class ServerApplication {
 
     public static void main(String[] args) {
