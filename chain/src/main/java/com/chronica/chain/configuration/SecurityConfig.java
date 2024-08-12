a63efc6c40f8178bdf8ac3b1f20da9829bb0a15a
BenzoEnzo <goxer@o2.pl>
2024-08-12 04:31:41 +0200

    chain authorization

---
+++ chain/src/main/java/com/chronica/chain/configuration/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.chronica.chain.configuration;
+
+import com.chronica.user.logic.security.JWTHandler;
+import com.chronica.user.logic.security.RequestAuthenticator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new RequestAuthenticator(new JWTHandler()), UsernamePasswordAuthenticationFilter.class)
+                .csrf(CsrfConfigurer::disable);
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/chains/**")
+                .hasRole("USER")
+                .requestMatchers(HttpMethod.POST, "/api/chains/**")
+                .hasRole("USER")
+                .requestMatchers(HttpMethod.PUT, "/api/chains/**")
+                .hasRole("USER")
+                .requestMatchers(HttpMethod.DELETE, "/api/chains/**")
+                .hasRole("SYSTEM")
+                .requestMatchers(HttpMethod.GET, "/h2-console/**", "/swagger-ui/**", "/v3/**")
+                .hasRole("SYSTEM")
+                .anyRequest()
+                .authenticated()
+        );
+
+        return http.getOrBuild();
+    }
+
+    @Bean
+    protected WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**")
+                .requestMatchers("/swagger-ui/**")
+                .requestMatchers("/v3/**");
+    }
+}
+
