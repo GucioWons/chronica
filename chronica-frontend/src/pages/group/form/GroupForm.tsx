69ce5d85f27595974432a4cecda6d015b21c6832
GucioWons <wgutek1@wp.pl>
2024-09-01 21:33:07 +0200

    added-backend-requests-for-creation-and-edition

--- chronica-frontend/src/pages/group/form/GroupForm.tsx
+++ chronica-frontend/src/pages/group/form/GroupForm.tsx
@@ -1,11 +1,15 @@
 import Form from "../../../shared/form/Form";
 import {DTOs} from "../../../shared/dto/dtos";
 import FormInput from "../../../shared/form/FormInput";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import {UseFormReturn} from "react-hook-form";
 import FormSelect from "../../../shared/form/FormSelect";
 import GroupDTO = DTOs.GroupDTO;
 import Category = DTOs.Category;
+import axios from "axios";
+import {groupsApi} from "../../../shared/apiConstants";
+import {useNavigate} from "react-router";
+import { toast } from "react-toastify";
 
 export interface GroupFormProps {
     group?: GroupDTO
@@ -14,11 +18,37 @@ export interface GroupFormProps {
 function GroupForm(props: GroupFormProps) {
     const { group } = props;
 
+    const editMode: boolean = !!group;
+
+    const navigate = useNavigate();
+
     const [ formMethods, setFormMethods ] = useState<UseFormReturn<GroupDTO> | null>(null);
 
     const onSubmit = (data: GroupDTO) => {
-        console.log(data);
+        if (editMode) {
+            handleCreation(data);
+        } else {
+            handleEdition(data);
+        }
     }
+
+    const handleCreation = useCallback((data: GroupDTO) => {
+        axios.post<GroupDTO>(groupsApi + "/groups", data)
+            .then((response) => {
+                toast.success("Successfully created group!");
+                navigate("group/" + response.data.id);
+            })
+            .catch(() => toast.error("Could not create group!"));
+    }, []);
+
+    const handleEdition = useCallback((data: GroupDTO) => {
+        axios.put<GroupDTO>(groupsApi + `/groups/${data.id}`, data)
+            .then((response) => {
+                toast.success("Successfully updated group!");
+                navigate("group/" + response.data.id);
+            })
+            .catch(() => toast.error("Could not update group!"));
+    }, []);
 
     return (
         <Form
