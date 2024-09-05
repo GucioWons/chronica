20a5f2b943e3f141008614db3cc3b82957840b71
DELL <goxer@o2.pl>
2024-09-05 19:31:52 +0200

    majkel cors torebeczki

--- gateway/src/main/java/com/chronica/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/chronica/gateway/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,6 +24,16 @@ public class SecurityConfig {
         );
 
         return http.getOrBuild();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:9000");
+            }
+        };
     }
 
 }
