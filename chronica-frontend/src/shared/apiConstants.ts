648ae65ff0d0e50116e504caaf91a668d7ecd70a
GucioWons <wgutek1@wp.pl>
2024-09-06 18:24:05 +0200

    Corrections

--- chronica-frontend/src/shared/apiConstants.ts
+++ chronica-frontend/src/shared/apiConstants.ts
@@ -1,3 +1,4 @@
 export const accountsApi = "http://localhost:9000/api/accounts"
 export const linksApi = "http://localhost:9000/api/links"
 export const groupsApi = "http://localhost:9000/api/groups"
+export const projectsApi = "http://localhost:9000/api/projects"
