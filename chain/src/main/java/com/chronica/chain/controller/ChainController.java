1ee074c5d5219f37cbb7e34df3525781b9f57b6f
GucioWons <wgutek1@wp.pl>
2024-09-15 14:14:07 +0200

    Added-chain-options

--- chain/src/main/java/com/chronica/chain/controller/ChainController.java
+++ chain/src/main/java/com/chronica/chain/controller/ChainController.java
@@ -1,8 +1,10 @@
 package com.chronica.chain.controller;
 
+import com.chronica.chain.logic.ChainOptionsService;
 import com.chronica.chain.logic.ChainService;
 import lombok.RequiredArgsConstructor;
 import org.chronica.library.dto.chain.ChainDTO;
+import org.chronica.library.dto.chain.ChainSelectDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChainController {
     private final ChainService chainService;
+    private final ChainOptionsService chainOptionsService;
 
     @PostMapping
     public ResponseEntity<ChainDTO> createChain(@RequestBody ChainDTO chain) {
@@ -42,6 +45,13 @@ public class ChainController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(chainService.updateChainById(chainId, chainDto));
+    }
+
+    @GetMapping("/options")
+    public ResponseEntity<List<ChainSelectDTO>> getChainsOptions() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(chainOptionsService.getChainSelects());
     }
 
     @DeleteMapping("/{chainId}")
