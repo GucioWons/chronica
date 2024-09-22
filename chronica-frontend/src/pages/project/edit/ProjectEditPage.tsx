35a5e9333df36b53ba819ec318c646f845f5e0db
GucioWons <wgutek1@wp.pl>
2024-09-22 16:12:10 +0200

    Handling-project-errors

--- chronica-frontend/src/pages/project/edit/ProjectEditPage.tsx
+++ chronica-frontend/src/pages/project/edit/ProjectEditPage.tsx
@@ -6,17 +6,18 @@ import ProtectedPage from "../../../shared/ProtectedPage";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {projectsApi} from "../../../shared/apiConstants";
+import {useErrorHandler} from "../../../shared/http/handleError";
 
 function ProjectEditPage() {
     const { id } = useParams<{ id: string }>();
 
     const [project, setProject] = useState<ProjectDTO>();
-    const navigate = useNavigate();
+    const handleError = useErrorHandler();
 
     useEffect(() => {
         axios.get<ProjectDTO>(`${projectsApi}/${id}`)
             .then(data => setProject(data.data))
-            .catch(() => navigate(-1));
+            .catch((error) => handleError(error));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
