dffdc1ee4c8665d703700f8147376a625143115b
GucioWons <wgutek1@wp.pl>
2025-01-16 21:28:08 +0100

    Navbar-styling

--- chronica-frontend/src/shared/Navbar.tsx
+++ chronica-frontend/src/shared/Navbar.tsx
@@ -1,5 +1,7 @@
 import {useAuth} from "../context/useAuth";
 import { useNavigate } from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faBell, faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     const { isLoggedIn, account, logoutUser } = useAuth()
@@ -18,14 +20,14 @@ function Navbar() {
             <div style={{
                 display: "flex",
                 justifyContent: "flex-start",
-                flex: 2
+                flex: 1
             }}>
-                MENU
+                <FontAwesomeIcon icon={faBars} />
             </div>
             <div style={{
                 display: "flex",
                 justifyContent: "center",
-                flex: 1
+                flex: 2
             }}>
                 CHRONICA
             </div>
@@ -36,22 +38,26 @@ function Navbar() {
             }}
             onClick={handleNotificaitons}
             >
-                NOTIFICATIONS
+                <FontAwesomeIcon icon={faBell} />
             </div>
-            <div style={{
+            <div className="input-input" style={{
                 display: "flex",
                 justifyContent: "center",
                 flex: 1
             }}>
-                <input/>
+                <input placeholder="Search"/>
             </div>
             <div style={{
                 display: "flex",
-                justifyContent: "center",
+                justifyContent: "space-around",
                 flex: 1
             }}>
-                {account?.username}
-                <button onClick={logoutUser}>logout</button>
+                <div>
+                    {account?.username}
+                </div>
+                <div>
+                    <FontAwesomeIcon onClick={logoutUser} icon={faRightFromBracket} />
+                </div>
             </div>
         </div>
     )
